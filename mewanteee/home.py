f84571ad78422aa860b692772ad48fbd6c4aa9cc
lucasw <wsacul@gmail.com>
2013-08-03 05:34:38 -0700

    Didn't change output, but made it use an html file as a base.
    Next add account creation request- emails would go to me , and at admin interface I would activate accounts, which then could login.
    
    From 1/5/09

--- mewanteee/home.py
+++ mewanteee/home.py
@@ -1,33 +1,25 @@
-print 'Content-Type: text/html'
-print ''
-print '<center><b>ME WANTEE!</b></center>'
-print '<br><br><br><br><br><br><br>'
-print '<br><br><br><br><br><br><br>'
-print '<br><br><br><br><br><br><br>'
+import cgi
+import wsgiref.handlers
+import os
 
-# google analytics
-print """
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-1790845-3");
-pageTracker._trackPageview();
-</script>
-"""
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 
-# ads
-print """
-<script type="text/javascript"><!--
-google_ad_client = "pub-3567472245202712";
-/* 728x90, created 9/11/08 */
-google_ad_slot = "7554612788";
-google_ad_width = 728;
-google_ad_height = 90;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-"""
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
+
+		template_values = { }
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, template_values))
+
+def main():
+
+	application = webapp.WSGIApplication([ 
+		('/', MainPage),	
+		],
+		debug=True)
+	wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+	main()
